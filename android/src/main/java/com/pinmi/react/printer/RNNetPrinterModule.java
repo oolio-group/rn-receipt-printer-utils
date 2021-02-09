e2c3b322c6135a1ab65f2acf5f755c49e0f93eae
lakshmaji-till <66242708+lakshmaji-till@users.noreply.github.com>
2021-02-09 10:35:43 +0530

    Refactor changes (#4)
    * added log statement
    
    * trying with bytes
    
    * typo byte
    
    * byteData method
    
    * typo errors
    
    * missing impl
    
    * new String
    
    * use local var
    
    * missing build
    
    * ignore base64 conversion
    
    * build
    
    * Publish npm package
    
    * remove un-used transformations

--- android/src/main/java/com/pinmi/react/printer/RNNetPrinterModule.java
+++ android/src/main/java/com/pinmi/react/printer/RNNetPrinterModule.java
@@ -66,11 +66,6 @@ public class RNNetPrinterModule extends ReactContextBaseJavaModule implements RN
     @ReactMethod
     @Override
     public void printByteData(byte[] base64Data, Callback errorCallback) {
-//        Log.e(LOG_TAG, "bello ::  compaibilit start to print raw data ", ((Throwable)rawBase64Data).toString());
-//        String str = new String(base64Data);
-//        Log.v(LOG_TAG, "bello :: int base64 data inside netprintermodules ", str);
-        Log.e(LOG_TAG, "bello :: int bytes.length" + base64Data.length);
-        Log.e(LOG_TAG, "bello :: int bytes" + Arrays.toString(base64Data));
         adapter.printByteData(base64Data, errorCallback);
     }
 
