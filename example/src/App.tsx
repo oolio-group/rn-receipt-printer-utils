3c96205200962380a12d02197f0ea30b6ea1be8e
Lakshmaji <lakshmaji@tillpos.com.au>
2021-02-11 18:34:26 +0530

    Update example with sample buffer

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,200 +1,128 @@
-import * as React from "react";
 import {
   StyleSheet,
   View,
   Text,
-  Button,
-  Picker,
   TextInput,
+  TouchableOpacity,
 } from "react-native";
-import {
-  BLEPrinter,
-  NetPrinter,
-  USBPrinter,
-  IUSBPrinter,
-  IBLEPrinter,
-  INetPrinter,
-} from "rn-receipt-printer-utils";
-import Loader from "./Loader";
+import { NetPrinter } from "@tillpos/rn-receipt-printer-utils";
+import { Buffer } from "buffer";
+import React, {
+  Fragment,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 
-const printerList: Record<string, any> = {
-  ble: BLEPrinter,
-  net: NetPrinter,
-  usb: USBPrinter,
+export const usePrinterConnection = () => {
+  const sendMessage = useCallback(async (buffer) => {
+    NetPrinter.print(buffer.toString('base64'));
+  }, []);
+
+  return useMemo(
+    () => ({
+      sendMessage,
+    }),
+    [sendMessage]
+  );
 };
 
-interface SelectedPrinter
-  extends Partial<IUSBPrinter & IBLEPrinter & INetPrinter> {
-  printerType?: keyof typeof printerList;
+interface Printer {
+  device_name: string;
+  host: string;
+  port: number;
 }
+const PRINTERS: Array<Printer> = [
+  { device_name: "P1", host: "192.168.0.8", port: 9100 },
+];
 
 export default function App() {
-  const [selectedValue, setSelectedValue] = React.useState<
-    keyof typeof printerList
-  >("ble");
-  const [devices, setDevices] = React.useState([]);
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const [selectedPrinter, setSelectedPrinter] = React.useState<SelectedPrinter>(
-    {}
-  );
+  const [printers, setPrinters] = useState<Array<Printer>>([]);
+  const [currentPrinter, setCurrentPrinter] = useState();
+  const { sendMessage } = usePrinterConnection();
 
-  React.useEffect(() => {
-    const getListDevices = async () => {
-      const Printer = printerList[selectedValue];
-      // get list device for net printers is support scanning in local ip but not recommended
-      if (selectedValue === "net") return;
-      try {
-        setLoading(true);
-        await Printer.init();
-        const results = await Printer.getDeviceList();
-        setDevices(
-          results.map((item: any) => ({ ...item, printerType: selectedValue }))
-        );
-      } catch (err) {
-        console.warn(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getListDevices();
-  }, [selectedValue]);
-
-  const handleConnectSelectedPrinter = () => {
-    if (!selectedPrinter) return;
-    const connect = async () => {
-      try {
-        setLoading(true);
-        switch (selectedPrinter.printerType) {
-          case "ble":
-            await BLEPrinter.connectPrinter(
-              selectedPrinter?.inner_mac_address || ""
-            );
-            break;
-          case "net":
-            await NetPrinter.connectPrinter(
-              selectedPrinter?.host || "",
-              selectedPrinter?.port || ""
-            );
-            break;
-          case "usb":
-            await USBPrinter.connectPrinter(
-              selectedPrinter?.vendor_id || "",
-              selectedPrinter?.product_id || ""
-            );
-            break;
-          default:
-        }
-      } catch (err) {
-        console.warn(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    connect();
+  const _connectPrinter = (host: string, port: number) => {
+    //connect printer
+    NetPrinter.connectPrinter(host, port).then(
+      (printer: any) => setCurrentPrinter(printer),
+      (error: any) => console.warn(error)
+    );
   };
 
-  const handlePrint = async () => {
+  const initDevices = async () => {
     try {
-      const Printer = printerList[selectedValue];
-      await Printer.printText("<C>sample text</C>\n");
+      await NetPrinter.init();
+      setPrinters(PRINTERS);
     } catch (err) {
       console.warn(err);
     }
   };
 
-  const handleChangePrinterType = async (type: keyof typeof printerList) => {
-    setSelectedValue((prev) => {
-      printerList[prev].closeConn();
-      return type;
-    });
-    setSelectedPrinter({});
-  };
+  useEffect(() => {
+    initDevices();
+  }, []);
 
-  const handleChangeHostAndPort = (params: string) => (text: string) =>
-    setSelectedPrinter((prev) => ({
-      ...prev,
-      device_name: "Net Printer",
-      [params]: text,
-      printerType: "net",
-    }));
+  const testPrint = useCallback(async () => {
+    if (currentPrinter) {
+      const buf = Buffer.from('Minions');
 
-  const _renderNet = () => (
-    <View style={{ paddingVertical: 16 }}>
-      <View style={styles.rowDirection}>
-        <Text>Host: </Text>
-        <TextInput
-          placeholder="192.168.100.19"
-          onChangeText={handleChangeHostAndPort("host")}
-        />
-      </View>
-      <View style={styles.rowDirection}>
-        <Text>Port: </Text>
-        <TextInput
-          placeholder="9100"
-          onChangeText={handleChangeHostAndPort("port")}
-        />
-      </View>
-    </View>
-  );
-
-  const _renderOther = () => (
-    <Picker selectedValue={selectedPrinter} onValueChange={setSelectedPrinter}>
-      {devices.map((item: any, index) => (
-        <Picker.Item
-          label={item.device_name}
-          value={item}
-          key={`printer-item-${index}`}
-        />
-      ))}
-    </Picker>
-  );
+      await sendMessage(buf);
+    }
+  }, [sendMessage, currentPrinter]);
 
   return (
     <View style={styles.container}>
-      <View style={styles.section}>
-        <Text>Select printer type: </Text>
-        <Picker
-          selectedValue={selectedValue}
-          onValueChange={handleChangePrinterType}
+      {printers.map((printer, i) => (
+        <TouchableOpacity
+          key={i}
+          style={{
+            padding: 10,
+            backgroundColor: "#fff",
+            margin: 10,
+            borderColor: "#000",
+            borderWidth: 1,
+          }}
+          onPress={() => _connectPrinter(printer.host, printer.port)}
         >
-          {Object.keys(printerList).map((item, index) => (
-            <Picker.Item
-              label={item.toUpperCase()}
-              value={item}
-              key={`printer-type-item-${index}`}
-            />
-          ))}
-        </Picker>
-      </View>
-      <View style={styles.section}>
-        <Text>Select printer: </Text>
-        {selectedValue === "net" ? _renderNet() : _renderOther()}
-      </View>
-      <Button
-        disabled={!selectedPrinter?.device_name}
-        title="Connect"
-        onPress={handleConnectSelectedPrinter}
-      />
-      <Button
-        disabled={!selectedPrinter?.device_name}
-        title="Print sample"
-        onPress={handlePrint}
-      />
-      <Loader loading={loading} />
+          <Text>
+            {" "}
+            {`device_name: ${printer.device_name}, host: ${printer.host}, port: ${printer.port}`}
+          </Text>
+        </TouchableOpacity>
+      ))}
+
+      {!currentPrinter && <Text>Select a printer</Text>}
+      {currentPrinter && (
+        <Fragment>
+          <TouchableOpacity
+            onPress={testPrint}
+            style={{ padding: 10, backgroundColor: "#ccc", margin: 10 }}
+          >
+            <Text> Print receipt </Text>
+          </TouchableOpacity>
+        </Fragment>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    // backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: "absolute",
+    right: 0,
+  },
+  body: {
+    // backgroundColor: Colors.white,
+  },
   container: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 16,
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
-  section: {
-    flex: 1,
-  },
-  rowDirection: {
-    flexDirection: "row",
+  highlight: {
+    fontWeight: "700",
   },
 });
