2317a26e79b7ee19569f1f8f9687ed39693992c9
Md <siddiq@tillpos.com.au>
2021-09-19 23:54:16 +0530

    Add sdk adapters for different brand sdks

---
+++ ios/adapters/epson/USBPrinterEpsonAdapter.h
@@ -0,0 +1,21 @@
+//
+//  USBPrinterEpsonAdapter.h
+//
+//  Created by Till POS on 14/09/21.
+//
+
+
+#ifndef USBPrinterEpsonAdapter_h
+#define USBPrinterEpsonAdapter_h
+
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+@interface USBPrinterEpsonAdapter : NSObject
+
+@end
+
+#endif /* USBPrinterEpsonAdapter_h */
